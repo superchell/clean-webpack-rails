06307c895648c6b0c8f86c4e7e1c1a739a2cf18c
superchel <for.oleg.mozolev@gmail.com>
2018-01-24 16:07:29 +0200

    changed webpack component generator

--- lib/generators/component_generator.rb
+++ lib/generators/component_generator.rb
@@ -2,23 +2,34 @@ class ComponentGenerator < Rails::Generators::Base
   argument :component_name, required: true, desc: "Component name, e.g: button"
 
   def create_view_file
-    create_file "#{component_path}/_#{component_name}.html.erb"
+    create_file "#{component_path}/_#{file_name}.html.erb"
   end
 
   def create_css_file
-    create_file "#{component_path}/#{component_name}.css"
+    create_file "#{component_path}/#{file_name}.css"
   end
 
   def create_js_file
-    create_file "#{component_path}/#{component_name}.js" do
+    create_file "#{component_path}/#{file_name}.js" do
       # require component's CSS inside JS automatically
-      "import \"./#{component_name}.css\";\n"
+      "import \"./#{file_name}.css\";\n"
     end
   end
 
   protected
 
   def component_path
-    "frontend/components/#{component_name}"
+    path = "frontend/components/"
+    # build subdirectories path
+    if component_name.count("/") > 0
+      subdir_arr = component_name.split('/')
+      path = "#{path}#{subdir_arr.slice(0, subdir_arr.count - 1 ).join('/')}/"
+    end
+
+    "#{path}#{file_name}"
+  end
+
+  def file_name
+    component_name.split("/").last
   end
 end
